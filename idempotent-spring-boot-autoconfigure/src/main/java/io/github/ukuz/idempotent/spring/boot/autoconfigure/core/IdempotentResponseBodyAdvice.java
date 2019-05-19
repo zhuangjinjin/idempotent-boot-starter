37397c46b9a46085ad75d2f470a78c1d77930dc6
庄津津 <zhuangjinjin90@gmail.com>
2019-05-19 14:47:30 +0800

    Add idempotent response body advice

---
+++ idempotent-spring-boot-autoconfigure/src/main/java/io/github/ukuz/idempotent/spring/boot/autoconfigure/core/IdempotentResponseBodyAdvice.java
@@ -0,0 +1,11 @@
+package io.github.ukuz.idempotent.spring.boot.autoconfigure.core;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+/**
+ * @author ukuz90
+ * @date 2019-05-19
+ */
+public class IdempotentAdvice implements ResponseBodyAdvice<> {
+}
---
+++ idempotent-spring-boot-autoconfigure/src/test/java/io/github/ukuz/idempotent/spring/boot/autoconfigure/core/IdempotentResponseBodyAdviceTestApplication.java
@@ -0,0 +1,8 @@
+package io.github.ukuz.idempotent.spring.boot.autoconfigure.core;
+
+/**
+ * @author ukuz90
+ * @date 2019-05-19
+ */
+public class IdempotentResponseBodyAdviceTestApplication {
+}
